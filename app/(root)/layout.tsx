46f6b25bb1c2f7d03c3f815c6e05909ba8e81eb9
Sadiya-025 <adibasadiya9502@gmail.com>
2025-04-19 00:16:28 +0530

    Initial Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.png" alt="Logo" width={38} height={32} />
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
           <h2 className="text-primary-100">Avatar</h2>
         </Link>
       </nav>
